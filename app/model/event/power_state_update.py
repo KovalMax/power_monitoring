c546376210587208134ab5ec258ae9fdcde1f7df
kovalmax <kovalmax5@gmail.com>
2023-05-17 14:59:36 +0300

    fix json

--- app/model/event/power_state_update.py
+++ app/model/event/power_state_update.py
@@ -1,16 +1,5 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-from app.model.enum.power_state import PowerStateEnum
+from app.model.request.track_request import TrackRequest
 
 
-class PowerStateUpdateEvent(BaseModel):
-    device_id: str
-    state: PowerStateEnum
-    fired_at: datetime
-
-    class Config:
-        json_encoders = {
-            datetime: lambda v: v.timestamp()
-        }
+class PowerStateUpdateEvent(TrackRequest):
+    pass
