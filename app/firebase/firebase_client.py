d7fb8bc9246a4db3418183a005c363decf48544d
kovalmax <kovalmax5@gmail.com>
2023-05-29 21:25:41 +0300

    fix query operator

--- app/firebase/firebase_client.py
+++ app/firebase/firebase_client.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 from google.cloud.firestore_v1 import FieldFilter
 from google.cloud.firestore_v1.types import StructuredQuery
 
-from app.redis.redis_client import get_key, set_key
+from app.redis.redis_client import get_device, set_new_device
 
 cred = credentials.Certificate(environ.get('FIREBASE_KEY'))
 default_app = firebase_admin.initialize_app(cred)
@@ -14,15 +14,15 @@ db: firestore.firestore.Client = firestore.client()
 
 
 def device_exists(device_id: str) -> bool:
-    if get_key(device_id):
+    if get_device(device_id):
         return True
 
-    device_filter = FieldFilter('device_id', StructuredQuery.FieldFilter.Operator.EQUAL.value, device_id)
+    device_filter = FieldFilter('device_id', '==', device_id)
     query = db.collection('Devices').where(filter=device_filter).limit(1)
     snapshot = query.stream()
 
     exists = sum(1 for _ in snapshot) > 0
     if exists:
-        set_key(device_id, True)
+        set_new_device(device_id)
 
     return exists
--- app/redis/redis_client.py
+++ app/redis/redis_client.py
@@ -5,9 +5,9 @@ import redis
 redis = redis.Redis(host=environ.get("REDIS_HOST"), port=environ.get("REDIS_PORT"), db=environ.get("REDIS_INDEX"))
 
 
-def get_key(key: str):
-    return redis.get(key)
+def get_device(device_id: str) -> bool | None:
+    return redis.get(f'devices:${device_id}')
 
 
-def set_key(key: str, value: bool):
-    return redis.set(key, value)
+def set_new_device(device_id: str, value: bool = True):
+    return redis.set(f'devices:${device_id}', value)
