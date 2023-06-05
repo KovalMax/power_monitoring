58317a648ba8df627271f28505c02eccd816f669
kovalmax <kovalmax5@gmail.com>
2023-06-05 10:44:31 +0300

    refactoring

--- app/queue/worker.py
+++ app/queue/worker.py
@@ -1,7 +1,6 @@
 import dramatiq
 
 from app.dependencies.dependecies import get_event_service, get_device_service, get_rabbitmq
-from app.model.domain.event_model import EventModel
 from app.model.event.power_state_update import PowerStateUpdateEvent
 from app.service.device_service import DeviceService
 from app.service.event_service import EventService
@@ -15,11 +14,8 @@ device_service: DeviceService = get_device_service()
 
 @dramatiq.actor
 def power_state_update(update_event: dict[str, str | float]):
-    e = PowerStateUpdateEvent(**update_event)
-    if not device_service.device_exists(e.device_id):
+    event_model = PowerStateUpdateEvent(**update_event)
+    if not device_service.device_exists(event_model.device_id):
         return
 
-    model = EventModel(device_id=e.device_id, power_state=e.power_state, network_level=e.network_level,
-                       battery_level=e.battery_level, created_at=e.fired_at, updated_at=e.fired_at)
-
-    event_service.create_new_event(model)
+    event_service.create_new_event(event_model)
--- app/service/event_service.py
+++ app/service/event_service.py
@@ -2,6 +2,7 @@ import uuid
 
 from app.firebase.firebase_client import FireBaseClient
 from app.model.domain.event_model import EventModel
+from app.model.event.power_state_update import PowerStateUpdateEvent
 from app.redis.redis_client import RedisClient
 
 
@@ -10,5 +11,8 @@ class EventService:
         self.redis_client = redis_client
         self.firebase_client = firebase_client
 
-    def create_new_event(self, event: EventModel) -> None:
-        self.firebase_client.client.collection('Events').document(str(uuid.uuid4())).set(event.to_dict())
+    def create_new_event(self, event: PowerStateUpdateEvent) -> None:
+        model = EventModel(device_id=event.device_id, power_state=event.power_state, network_level=event.network_level,
+                           battery_level=event.battery_level, created_at=event.fired_at, updated_at=event.fired_at)
+
+        self.firebase_client.client.collection('Events').document(str(uuid.uuid4())).set(model.to_dict())
