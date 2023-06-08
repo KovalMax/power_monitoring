414a8023c4059d879f69f0efe76a2d0c3b669314
kovalmax <kovalmax5@gmail.com>
2023-06-08 23:05:19 +0300

    fix

--- app/controller/telegram.py
+++ app/controller/telegram.py
@@ -14,16 +14,10 @@ notification_service = get_notification_service()
 
 @router.post("/telegram", status_code=status.HTTP_200_OK)
 async def create_state_event(payload: dict = Body()):
-    print(type(payload))
     print(payload)
 
-    return {}
-
-    user_id = json['text'].split(' ')[1]
-    chat_id = json['chat']['id']
-
-    print(user_id)
-    print(chat_id)
+    user_id = payload['message']['text'].split(' ')[1]
+    chat_id = payload['message']['chat']['id']
 
     notification_service.save_chat_settings(user_id, chat_id)
     notification_service.send_telegram_message(chat_id, 'Сповіщення для телеграму налаштовано! U+2705')
