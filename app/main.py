272cc9f12b770f61b015532a9ccee38c23c9c78c
kovalmax <kovalmax5@gmail.com>
2023-06-08 16:57:14 +0300

    add

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from .controller import events
+from .controller import events, telegram
 
 app = FastAPI()
 app.include_router(events.router)
+app.include_router(telegram.router)
